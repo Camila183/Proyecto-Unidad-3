676b547f8e43c359e6702f1cd76af0d443a0516a
camilaaranguri5-ctc <camilaaranguri5@gmail.com>
2024-09-14 20:10:24 -0500

    cambio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,56 @@
-$(document).ready(function() {
-    const apikey = 'efb82061'; 
-    const query = obtenerConsulta(); 
-    const url = `https://www.omdbapi.com/?apikey=${apikey}&s=${query}`;
+function generateUrl() {
+    let id = Math.round(1000000 + Math.random() * 9000000);
+    return `https://www.omdbapi.com/?i=tt${id}&plot=short&type=movies&apikey=c42371fd`;
+}
 
+
+function getPic() {
     $.ajax({
-        type: 'GET',
-        url: url,
-        datatype: 'json',
-        async: true,
-        success: function(data) {
-            
-                mostrarPeliculas(data.Search);
-           
-        },
-        error: function(error) {
-            console.log("Error en la solicitud", error);
-        }
-        
-        
+    url: generateUrl(),
+    method: 'GET',
+    dataType: 'json',
+    success: function(info) {
+    if (info.Poster !== 'N/A' && info.Type === 'movie') {
+    renderPic(info);
+    } else {
+    getPic(); // Reintentar si no hay película o cartel válido
+    }
+    },
+    error: function(error) {
+    console.error('Error al obtener la película:', error);
+    }
     });
-
-    function obtenerConsulta() {
-        
-        let consulta = prompt("Ingresa el término de búsqueda:", "Barbie");
-        return consulta 
     }
 
-    function mostrarPeliculas(peliculas) {
-      
-        console.log(peliculas)
-        const moviesContainer = $('#movies-container');
-        moviesContainer.empty(); 
-
-        peliculas.forEach(function(pelicula) {
-            const poster = pelicula.Poster ; 
-            const movieTitle = pelicula.Title ;
-
-           
-            const movieDiv = `
-                <div class="movie">
-                    <img src="${poster}" alt="${movieTitle}">
-                    <div class="movie-title">${movieTitle}</div>
-                </div>
-            `;
-            moviesContainer.append(movieDiv);
-        });
+$(document).ready(function() {
+    for (let index = 1; index < 9; index++) {
+        getPic();
     }
-
-    
- 
-    
 });
+function renderPic(data) {
+    console.log(data);
+   
+    mostrarDatos(data);
+}
+
+function mostrarDatos(info) {
+    console.log(info);
+
+    const nuevaInfo = info;
+
+   
+    let posterDiv = $("<div></div>").addClass("recomendaciones");
+
+   
+    $("<img>").attr("src", nuevaInfo.Poster).appendTo(posterDiv);
+
+   
+    $("<h3></h3>").text(nuevaInfo.Title).appendTo(posterDiv);
+    $("<p></p>").text(nuevaInfo.Actors).appendTo(posterDiv);
+
+
+    $("#recomendaciones").append(posterDiv);
+}
+
+
+
